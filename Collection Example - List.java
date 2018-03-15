366a0c3a60c7c3612bf407b83cf49fbaf03b4ad3
MichaelCrump <MichaelCrump@users.noreply.github.com>
2018-03-15 17:18:25 -0600

    Add files via upload

---
+++ Collection Example - List.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Fabric {
+  public static void main(String[] args) {
+  List<String> list_Strings = new ArrayList<String>();
+  list_Strings.add("Silk");
+  list_Strings.add("Satin");
+  list_Strings.add("Linen");
+  list_Strings.add("Wool");
+  list_Strings.add("Flannel");
+  System.out.println(list_Strings);
+ }
+}
