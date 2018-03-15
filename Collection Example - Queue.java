ff98644f6ab572756fe5ef74f4b5fdd42784bb83
MichaelCrump <MichaelCrump@users.noreply.github.com>
2018-03-15 17:33:58 -0600

    Add files via upload

---
+++ Collection Example - Queue.java
@@ -0,0 +1,11 @@
+Queue<String> queueFabric = new LinkedList<>();
+ 
+queueNames.add("Silk");
+queueNames.add("Satin");
+queueNames.add("Linen");
+queueNames.add("Wool");
+queueNames.add("Flannel");
+ 
+for (String name : queueFabric) {
+    System.out.println(name);
+} 
